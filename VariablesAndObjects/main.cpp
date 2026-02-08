f3cb5596c9591f4ffc83c618c9109feaf4d840d9
LenaDotGirl <aimedotgirl@gmail.com>
2026-02-08 17:32:18 +0100

    Added VariablesAndObjects

---
+++ VariablesAndObjects/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 123;
+    cout << "X is " << x << endl;
+    x = 456;
+    cout << "Now it's " << x << " •̀ ᵕ •́ " << endl;
+    return 0;
+}
