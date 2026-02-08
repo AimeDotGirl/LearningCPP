75c242ae292e61f3e0a9300ef2f34c1e9690e079
LenaDotGirl <aimedotgirl@gmail.com>
2026-02-08 18:05:10 +0100

    Added Operator

---
+++ Operator/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1 + 1 = " << 1 + 1 << endl;
+    cout << "1 - 2 = " << 1 - 2 << endl;
+    cout << "14 * 3 = " << 14 * 3 << endl;
+    cout << "15 / 5 = " << 15 / 5 << endl;
+    cout << "5 % 3 = " << 5 % 3 << endl;
+
+    cout << "1 + 1 - 2 + 14 * 3 + 15 / 5 + 5 % 3 = " << 1 + 1 - 2 + 14 * 3 + 15 / 5 + 5 % 3 << endl;
+
+    return 0;
+}
