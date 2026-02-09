cbfffdc329138437f3a1a65170393b222efa8953
LenaDotGirl <aimedotgirl@gmail.com>
2026-02-09 19:58:50 +0100

    Added Multiplier

---
+++ HelloEverybodyMyNameIsMultiplier/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+    int x {};
+    cin >> x;
+    cout << "Now it's " << x * 2 << endl;
+    return 0;
+}
